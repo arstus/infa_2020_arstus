46a3df6cc03ae7b1bfb69871b8ee771c2c833751
arstus <arstus@gmail.com>
2020-04-11 16:01:38 +0400

    Progres lab3

---
+++ lab3/t_1.py
@@ -0,0 +1,22 @@
+from graph import *
+
+brushColor("yellow")
+circle(250, 300, 150)
+
+brushColor("red")
+circle(180, 250, 30)
+brushColor("black")
+circle(180, 250, 10)
+
+brushColor("red")
+circle(320, 250, 20)
+brushColor("black")
+circle(320, 250, 10)
+
+rectangle(180, 370, 320, 390)
+polygon([(100, 170), (220, 240),
+         (225, 230), (105, 160), (100,170)])
+polygon([(400, 190), (280, 250),
+         (275, 240), (395, 180), (400,190)])
+
+run()
